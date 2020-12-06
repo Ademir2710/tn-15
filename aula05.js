2b0b6233edb0dc879a25a3ff4109f402c1fd06fa
Ademir2710 <ademir.i.goncalves@gmail.com>
2020-12-06 13:54:21 -0200

    criação de função mostrar paísdiversas

--- aula05.js
+++ aula05.js
@@ -8,6 +8,23 @@ function mostrarPais() {
     
 }
 
-
+var result = mostrarPais()
 mostrarNome("vitor")
+// console.log(result)
 mostrarPais()
+
+
+function writeName(a) {
+    return (a)
+    
+}
+
+var resultname = writeName("Ademirrrr")
+// console.log(resultname)
+
+function name(n1 , n2) {
+    return n1+n2
+}
+
+var soma = name(2,2)
+console.log(soma)
